8efa7a76f7da2b53452fcfa194074fb0289265a4
StarCode <iamstarcode@gmail.com>
2024-05-15 18:36:44 +0100

    chore: upated suppported coin

--- src/quidax/types/coins.ts
+++ src/quidax/types/coins.ts
@@ -1,15 +1,9 @@
-interface Functionality {
-  buySell: boolean;
-  sendReceive: boolean;
-  network?: string; // Optional network information
-}
-
 interface Currency {
   currency: string;
   supportedNetworks: string[];
   quoteTicker: string[];
-  features: string;
-  multipleAddress: boolean;
+  features?: string;
+  multipleAddress: boolean | null;
   canBuy?: boolean;
   canSell?: boolean;
   canReceive?: boolean;
@@ -20,479 +14,379 @@ const coins: { [symbol: string]: Currency } = {
     currency: 'Bitcoin',
     quoteTicker: ['usdt', 'ngn'],
     supportedNetworks: ['btc'],
-    features: 'Create multiple wallet addresses, Buy, Sell, Send, and Receive',
     multipleAddress: true,
   },
   ltc: {
     currency: 'Litecoin',
     quoteTicker: ['usdt', 'ngn'],
     supportedNetworks: ['ltc'],
-    features: 'Create multiple wallet addresses, Buy, Sell, Send, and Receive',
     multipleAddress: true,
   },
   dash: {
     currency: 'Dash',
     quoteTicker: ['usdt', 'ngn'],
     supportedNetworks: ['dash'],
-    features: 'Create multiple wallet addresses, Buy, Sell, Send, and Receive',
     multipleAddress: true,
   },
   trx: {
     currency: 'Tron',
     quoteTicker: ['usdt', 'ngn'],
     supportedNetworks: ['trc20'],
-    features: 'Buy, Sell, Send, and Receive, Create a single wallet address',
     multipleAddress: false,
   },
   qdx: {
     currency: 'Quidax Token',
     quoteTicker: ['usdt', 'ngn'],
     supportedNetworks: ['bep20'],
-    features: 'Buy, Sell, Send, and Receive, Create a single wallet address',
-    multipleAddress: true,
+    multipleAddress: false,
   },
   eth: {
     currency: 'Ethereum',
-    supportedNetworks: ['usdt', 'ngn'],
-    blockchain: 'erc20, bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt', 'ngn'],
+    supportedNetworks: ['erc20', ' bep20'],
+    multipleAddress: false,
   },
   xrp: {
     currency: 'Ripple',
-    supportedNetworks: ['usdt', 'ngn'],
-    blockchain: 'ripple',
-    features: 'Create wallet addresses, Buy, Sell, Send, and Receive',
+    quoteTicker: ['usdt', 'ngn'],
+    supportedNetworks: ['ripple'],
+    multipleAddress: true,
   },
   bch: {
     currency: 'Bitcoin Cash',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bch',
-    features: 'Create wallet addresses, Buy, Sell, Send, and Receive',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bch'],
+    multipleAddress: true,
   },
   sol: {
     currency: 'Solana',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   mana: {
     currency: 'Decentraland (MANA)',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   ftm: {
     currency: 'Fantom (FTM)',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
 
   sand: {
     currency: 'The Sandbox',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   floki: {
     currency: 'Floki Inu',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   xtz: {
     currency: 'Tezos',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   one: {
     currency: 'Harmony',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   ada: {
     currency: 'Cardano',
-    supportedNetworks: ['usdt'],
-    blockchain: 'cardano',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['cardano'],
+    multipleAddress: false,
   },
   babydoge: {
     currency: 'Babydoge',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   fil: {
     currency: 'Filecoin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   axs: {
     currency: 'Axie Infinity',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   xlm: {
     currency: 'Stellar',
-    supportedNetworks: ['usdt'],
-    blockchain: 'stellar',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['stellar'],
+    multipleAddress: false,
   },
   cake: {
     currency: 'PancakeSwap',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   link: {
     currency: 'Chainlink',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   dot: {
     currency: 'Polkadot',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   shib: {
     currency: 'Shiba Inu',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   aave: {
     currency: 'Aave',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   usdc: {
     currency: 'USD Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20, erc20, trc20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20', 'erc20', 'trc20'],
+    multipleAddress: false,
   },
   matic: {
     currency: 'Polygon',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20, erc20, trc20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20', 'erc20', 'trc20'],
+    multipleAddress: false,
   },
   bnb: {
     currency: 'Binance Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Send, and Receive, Create a single wallet address per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: false,
   },
   doge: {
     currency: 'Dogecoin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'doge',
-    features:
-      'Create wallet addresses, Buy, Sell, Send, and Receive per blockchain',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['doge'],
+    multipleAddress: true,
   },
   usdt: {
     currency: 'Tether',
-    supportedNetworks: ['ngn'],
-    blockchain: 'bep20, erc20, trc20',
-    features:
-      'Buy, Sell, Send, and Receive. Create a single wallet address per blockchain',
+    quoteTicker: ['ngn'],
+    supportedNetworks: ['bep20', 'erc20', 'trc20'],
+    multipleAddress: false,
   },
   meme: {
     currency: 'Memecoin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
 
   cnhc: {
     currency: 'CNHC Token',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
 
   cfx: {
     currency: 'Conflux',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   ape: {
     currency: 'Apecoin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   pepe: {
     currency: 'Pepecoin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   enj: {
     currency: 'Enjin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   lrc: {
     currency: 'Loopring',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   wld: {
     currency: 'Worldcoin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   arb: {
     currency: 'Arbitrum',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   algo: {
     currency: 'Algorand',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   ens: {
     currency: 'Ethereum Name Service',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
-  ordi: {
-    currency: 'Ordinals',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
-  },
-  meme: {
-    currency: 'MEME Coin (MEME)',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
-  },
+
   bonk: {
     currency: 'Bonk Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   sui: {
     currency: 'Sui Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   ordi: {
     currency: 'Ordi Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
-  ens: {
-    currency: 'Ethereum Name Service',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
-  },
-  algo: {
-    currency: 'Algorand Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
-  },
-  arb: {
-    currency: 'Arbitrum Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
-  },
+
   wif: {
     currency: 'Dogwifhat Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   myro: {
     currency: 'Myro Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   trump: {
     currency: 'MAGA',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   cob: {
     currency: 'Cobra King',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   virtual: {
     currency: 'Virtual Protocol',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   nos: {
     currency: 'Nosana Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   lpt: {
     currency: 'Livepeer Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   jup: {
     currency: 'Jupiter Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   blur: {
     currency: 'Blur Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   kata: {
     currency: 'Katana Inu Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   op: {
     currency: 'Optimism Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   naka: {
     currency: 'Nakamoto Games Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
 
-  naka: {
-    currency: 'Nakamoto Games Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
-  },
   icp: {
     currency: 'Internet Computer Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   kas: {
     currency: 'Kaspa Coin',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   coq: {
     currency: 'Coq Inu',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   jto: {
     currency: 'Jito',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   wassie: {
     currency: 'WASSIE',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
   fet: {
     currency: 'Fetch.ai',
-    supportedNetworks: ['usdt'],
-    blockchain: 'bep20',
-    features:
-      'Buy, Sell, Cannot Send or Receive, Can not create wallet address',
+    quoteTicker: ['usdt'],
+    supportedNetworks: ['bep20'],
+    multipleAddress: null,
   },
 };
 
